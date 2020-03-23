e0d9c92c4c66408dc6813dbbd3778114984bee38
Chaklam <chaklam@ait.asia>
2020-03-23 15:39:52 +0700

    move ipynb_checkpoints to gitignore

--- XProj/print_raw_example.py
+++
@@ -1,9 +0,0 @@
-from pyOpenBCI import OpenBCICyton
-
-def print_raw(sample):
-    print(sample.channels_data)
-
-#Set (daisy = True) to stream 16 ch 
-board = OpenBCICyton(port='/dev/ttyUSB0')
-
-board.start_stream(print_raw)
