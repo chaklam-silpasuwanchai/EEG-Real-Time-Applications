a11b120e4f198496e9cef13ac7f8ec42f37b87c8
Chaklam <chaklam@ait.asia>
2020-04-07 11:15:03 +0700

    Added new data

--- SSVEP/3-analysis/filterbankcspDecoding.py
+++ SSVEP/3-analysis/filterbankcspDecoding.py
@@ -16,6 +16,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
@@ -36,7 +38,7 @@ import seaborn as sns
 
 def decode(raw, event_id, tmin, tmax):
 
-    raw_filt = raw.copy().filter(5, 16, method='iir')
+    raw_filt = raw.copy().filter(4, 20, method='iir')
     
     epoch = helper.getEpochs(raw_filt, event_id, tmin=tmin, tmax=tmax)
         
@@ -62,32 +64,34 @@ def model(epoch):
     #print(X)
      #CSP will take in data in this form and create features of 2d
     y = epoch.events[:, -1]
+    #y = label_binarize(y,classes=[1,2,3])
+    #print(y)
 
     #print(y)
 
     clfs = OrderedDict()
     
     lda = LDA(shrinkage='auto', solver='eigen') #Regularized LDA
-    svc = SVC()
-    lr = LogisticRegression()
-    knn = KNeighborsClassifier(n_neighbors=3) #you would want to optimize
-    nb = GaussianNB()
-    rf = RandomForestClassifier(n_estimators=50, random_state=1)
+    svc = OneVsRestClassifier(SVC())
+    lr = OneVsRestClassifier(LogisticRegression())
+    knn = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=3)) #you would want to optimize
+    nb = OneVsRestClassifier(GaussianNB())
+    rf = OneVsRestClassifier(RandomForestClassifier(n_estimators=50, random_state=1))
     mdm = MDM()
     ts = TangentSpace()
     vec = Vectorizer()
     scale = Scaler(epoch.info)  #by default, CSP already does this, but if you use Vectorizer, you hve to do it before Vectorizing
-    csp = CSP(n_components=2, reg=0.3) #feature extraction, reg is used when data is not PD (positive definite)
+    csp = CSP(n_components=3, reg=0.3) #feature extraction, reg is used when data is not PD (positive definite)
 
     #clfs['Vectorizer + LDA'] = Pipeline([('Scaler', scale), ('Vectorizer', vec), ('Model', lda)])
     clfs['CSP + LDA'] = Pipeline([('CSP', csp), ('Model', lda)])
-    # clfs['CSP + SVC'] = Pipeline([('CSP', csp), ('Model', svc)])
-    # clfs['CSP + LR'] = Pipeline([('CSP', csp), ('Model', lr)])
-    # clfs['CSP + KNN'] = Pipeline([('CSP', csp), ('Model', knn)])
-    # clfs['CSP + NB'] = Pipeline([('CSP', csp), ('Model', nb)])
-    # clfs['CSP + RF'] = Pipeline([('CSP', csp), ('Model', rf)])
-    # clfs['Cov + MDM'] = Pipeline([('Cov', Covariances('oas')), ('Model', mdm)]) #oas is needed for non-PD matrix
-    # clfs['Cov + TS'] = Pipeline([('Cov', Covariances('oas')), ('Model', ts)]) #oas is needed for non-PD matrix
+    clfs['CSP + SVC'] = Pipeline([('CSP', csp), ('Model', svc)])
+    clfs['CSP + LR'] = Pipeline([('CSP', csp), ('Model', lr)])
+    clfs['CSP + KNN'] = Pipeline([('CSP', csp), ('Model', knn)])
+    clfs['CSP + NB'] = Pipeline([('CSP', csp), ('Model', nb)])
+    clfs['CSP + RF'] = Pipeline([('CSP', csp), ('Model', rf)])
+    clfs['Cov + MDM'] = Pipeline([('Cov', Covariances('oas')), ('Model', mdm)]) #oas is needed for non-PD matrix
+    #clfs['Cov + TS'] = Pipeline([('Cov', Covariances('oas')), ('Model', ts)]) #oas is needed for non-PD matrix
     # #not sure why TS is not working....
 
     auc = []
@@ -97,18 +101,12 @@ def model(epoch):
     cv = StratifiedShuffleSplit(n_splits=10, test_size=0.25, 
                             random_state=42)
 
-    #print(X)
-    print(y)
-
     for m in clfs:
         print("+", end="") #to know it's working, no newline
-        try:
-            res = cross_val_score(clfs[m], X, y, scoring='roc_auc', 
-                              cv=cv, n_jobs=-1)
-            auc.extend(res)
-            methods.extend([m]*len(res))
-        except:
-            pass
+        res = cross_val_score(clfs[m], X, y, n_jobs=-1)
+        auc.extend(res)
+        #print(auc)
+        methods.extend([m]*len(res))
     
     results = pd.DataFrame(data=auc, columns=['AUC'])
     results['Method'] = methods
@@ -118,6 +116,7 @@ def model(epoch):
 def plot(df):
     figure = plt.figure(figsize=[8,4])
     plt.title("AOC")
+    #print(df.head())
     sns.barplot(data=df, x='AUC', y='Method')
     plt.xlim(0.4, 1)    
     sns.despine()
